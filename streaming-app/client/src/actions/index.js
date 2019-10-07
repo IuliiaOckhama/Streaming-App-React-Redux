94e971c949a87af5f84a7c21139c5eca37fdedc8
Iuliia <code.enemy@gmail.com>
2019-10-07 10:44:52 +0300

    refactor forms, add redux thunk, axios, jsonplaceholder database

---
+++ streaming-app/client/src/actions/index.js
@@ -0,0 +1,19 @@
+import streams from '../api/streams'
+
+export const signIn = userId => {
+  return {
+    type: 'SIGN_IN',
+    payload: userId,
+  }
+}
+
+export const signOut = () => {
+  return {
+    type: 'SIGN_OUT',
+  }
+}
+export const createStream = formValues => {
+  return async (dispatch, getState) => {
+    streams.post('/streams', formValues)
+  }
+}
