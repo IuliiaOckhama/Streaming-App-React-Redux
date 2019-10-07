ab949b0a875962fd46f4b1b6d26444c2022e5d0d
Iuliia <code.enemy@gmail.com>
2019-10-07 16:02:25 +0300

    Add Edit/Delete buttons to the streams in StreamList component; add 'CreateStream' button

--- streaming-app/client/src/components/streamPages/StreamList.js
+++ streaming-app/client/src/components/streamPages/StreamList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { fetchStreams } from '../../actions'
 
@@ -6,10 +7,21 @@ class StreamList extends Component {
   componentDidMount() {
     this.props.fetchStreams()
   }
+  renderAdminButtons(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className='right floated content'>
+          <button className='ui button primary'>Edit</button>
+          <button className='ui button negative'>Delete</button>
+        </div>
+      )
+    }
+  }
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className='item' key={stream.id}>
+          {this.renderAdminButtons(stream)}
           <i className='large middle aligned icon camera' />
           <div className='content'>
             {stream.title}
@@ -19,17 +31,33 @@ class StreamList extends Component {
       )
     })
   }
+  renderCreateButton() {
+    if (this.props.isSignedIn) {
+      return (
+        <div>
+          <Link to='/streams/new' className='ui button primary'>
+            Create new stream
+          </Link>
+        </div>
+      )
+    }
+  }
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className='ui celled list'>{this.renderList()}</div>
+        {this.renderCreateButton()}
       </div>
     )
   }
 }
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) }
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  }
 }
 export default connect(
   mapStateToProps,
