00635db22c0d50df9a47d2d8d154e81563b73d82
Iuliia <code.enemy@gmail.com>
2019-10-07 12:42:36 +0300

    add stream reducers

--- streaming-app/client/src/reducers/index.js
+++ streaming-app/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
-import authReducer from './authReducer'
 import { reducer } from 'redux-form'
+import authReducer from './authReducer'
+import streamReducer from './streamReducer'
 
 export default combineReducers({
   auth: authReducer,
+  streams: streamReducer,
   form: reducer,
 })
