ff91f2fb11f07b2afdef7b9c691678d5e14e6ecf
Iuliia <code.enemy@gmail.com>
2019-10-09 13:00:55 +0300

    add rtmp server, add video player to the StreamShow component

--- streaming-app/client/src/components/streamPages/StreamShow.js
+++ streaming-app/client/src/components/streamPages/StreamShow.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import flv from 'flv.js'
 import { fetchStream } from '../../actions'
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props)
+    this.videoRef = React.createRef()
+  }
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
   }
@@ -16,6 +21,7 @@ class StreamShow extends Component {
     return (
       <div>
         <h1>{stream.title}</h1>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h3>{stream.description}</h3>
       </div>
     )
